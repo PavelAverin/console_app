7d5b8a3775ebdba2233a1b38513af40b035e03f1
Pavel Averin <averin.samara@gmail.com>
2022-08-12 09:11:33 +0300

    Выполнили 1 пункт из второго практического задания
    С помощью цикла автоматизировали процесс получения ответов от пользователя

--- js/script.js
+++ js/script.js
@@ -10,13 +10,12 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = +prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', '');
+  const b = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
 
