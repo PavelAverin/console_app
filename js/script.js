ce498a3ae6f7b3476aef9d9cb252b0ed0cc99497
Pavel Averin <averin.samara@gmail.com>
2022-08-15 00:13:50 +0300

    Выполнили второе практическое задание
    1. Автоматизировали вопросы пользователю через цикл
    2. С помощью условия ограничили количество ответов пользователя.
    3. И с помощью условий мы теперь можем оценить зрителя.

--- js/script.js
+++ js/script.js
@@ -12,11 +12,23 @@ const personalMovieDB = {
 
 for (let i = 0; i < 2; i++) {
   const a = prompt('Один из последних просмотренных фильмов?', '');
-  const b = +prompt('На сколько оцените его?', '');
+  const b = prompt('На сколько оцените его?', '');
 
-  personalMovieDB.movies[a] = b;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы класический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
-
-
